0ccaa90979af05cbe5d7fec0823823313d65cbc5
mohamad razavi <mohammadrazavi71@gmail.com>
2023-01-20 23:02:39 +0330

    start projekt

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import "./App.css";
+
+import Test from "./Test";
+
+
+const App = () => {
+   
+   
+    
+  return (
+    <div className="app">
+        <Test/>
+    </div>
+  );
+};
+
+export default App;
