7cfa2e04d7884f97a3ddcc28891f476e45c23e9c
mohammad razavi <mohammadrazavi71@gmail.com>
2023-01-29 15:22:18 +0330

    api done

---
+++ src/Fakeapi.js
@@ -0,0 +1,21 @@
+const questions = () => [
+    {
+        id : 1 ,
+        titel : "about me?",
+        info : "ich bin mohammad and 30 jahre alt"
+    }
+    ,
+    {
+        id : 2 ,
+        titel : "what is front end?",
+        info : "it is somethin im client and very gut"
+    }
+    ,
+    {
+        id : 3 ,
+        titel : "what is react?",
+        info : "react ist ein libary von javascript and user-interface"
+    }
+]
+
+export default questions;
